c592cfb40dcd428ca286ad424f712f125c491194
Guilherme Vaz <guilherme_vaz@sicredi.com.br>
2023-04-28 23:05:58 -0300

    projeto inicial, primeiro request

---
+++ src/app/shared/poke-list/poke-list.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { PokeApiService } from 'src/app/service/poke-api.service';
+
+@Component({
+  selector: 'poke-list',
+  templateUrl: './poke-list.component.html',
+  styleUrls: ['./poke-list.component.scss'],
+})
+export class PokeListComponent implements OnInit {
+  constructor(private pokiApiService: PokeApiService) {}
+
+  ngOnInit(): void {
+    this.pokiApiService.apiListAllPokemons.subscribe({
+      next: (res) => res,
+    });
+  }
+}
